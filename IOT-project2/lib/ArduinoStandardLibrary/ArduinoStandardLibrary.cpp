3943075ff47075600bc5eb6d58b36239153706a2
giuseppe gambacorta <gambacortagiuseppe@oulook.it>
2024-11-18 10:16:43 +0100

    StandardLibrary

---
+++ IOT-project2/lib/ArduinoStandardLibrary/ArduinoStandardLibrary.cpp
@@ -0,0 +1,156 @@
+#include "ArduinoStandardLibrary.h"
+
+
+#pragma region DigitalInput
+DigitalInput::DigitalInput(unsigned int pin, unsigned long threshold)
+    : pin(pin), value(0), oldValue(0), trigChanged(0), activationTimer(*new Timer(threshold))
+{
+    pinMode(pin, INPUT);
+};
+
+void DigitalInput::update()
+{
+    this->value = digitalRead(this->pin);
+    this->trigChanged = this->value != this->oldValue;
+    this->oldValue = this->value;
+};
+
+bool DigitalInput::isActive(){
+    return this->value;
+}
+
+bool DigitalInput::isChanged(){
+    return this->trigChanged;
+}
+#pragma endregion
+
+#pragma region DigitalOutput
+DigitalOutput::DigitalOutput(unsigned int pin)
+    : pin(pin), value(0)
+{
+    pinMode(pin, OUTPUT);
+};
+
+
+void DigitalOutput::update()
+{
+    digitalWrite(this->pin, this->value);
+};
+
+void DigitalOutput::turnOn()
+{
+    this->value = 1;
+};
+
+void DigitalOutput::turnOff()
+{
+    this->value = 0;
+};
+
+bool DigitalOutput::isActivated()
+{
+    return this->value;
+};
+#pragma endregion
+
+#pragma region Timer
+Timer::Timer(unsigned long timeDuration)
+    : oldTime(0), timeDuration(timeDuration), start(0)
+{
+};
+
+void Timer::active(bool start)
+{
+    this->start = start;
+    if (start)
+    {
+        this->oldTime = millis();
+    }
+};
+
+
+bool Timer::isTimeElapsed()
+{
+    if (this->start)
+    {
+        unsigned long currentTime = millis();
+        if (currentTime - this->oldTime >= this->timeDuration)
+        {
+            return true;
+        }
+    }
+    return false;
+};
+
+void Timer::reset()
+{
+    this->oldTime = 0;
+    this->start = 0;
+};
+#pragma endregion
+
+#pragma region AnalogInput
+AnalogInput::AnalogInput(unsigned int pin, unsigned int mapValue,unsigned int filterSize)
+    : pin(pin), value(0),  mapValue(mapValue),maxFilterSize(filterSize), filterCount(0)
+{
+    pinMode(pin, INPUT);
+};
+
+
+void AnalogInput::filterValue(int inputValue){
+    if(this->filterCount < this->maxFilterSize){
+        this->filterCount++;
+    }
+    float val_coef = 1.0 / float(this->filterCount);
+    this->value += (inputValue - this->value) * val_coef;
+}
+
+void AnalogInput::update(){
+    this->filterValue(analogRead(this->pin));
+    this->value = map(this->value, 0, 1023, 0, this->mapValue);
+}
+
+
+int AnalogInput::getValue(){
+    return this->value;
+}
+
+
+#pragma endregion
+
+#pragma region AnalogOutput
+
+AnalogOutput::AnalogOutput(unsigned int pin, unsigned int maxValue)
+    : pin(pin), value(0), maxValue(maxValue)
+{
+    pinMode(pin, OUTPUT);
+};
+
+
+void AnalogOutput::setValue(int value){
+    if(value > this->maxValue){
+        value = this->maxValue;
+    }
+}
+
+
+
+int AnalogOutput::getValue(){
+    return this->value;
+}
+
+
+void AnalogOutput::update(){
+    analogWrite(this->pin, map(this->value, 0, this->maxValue, 0, 255));
+}
+
+#pragma endregion
+
+
+
+
+
+
+
+
+
