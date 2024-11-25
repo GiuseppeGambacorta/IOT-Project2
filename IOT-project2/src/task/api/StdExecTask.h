28fe7441b5205ebf2ab4ab106df36b1a743e69b5
Filippo Massari <filippo.massari6@studio.unibo.it>
2024-11-25 10:03:05 +0100

    add I/O obj to sdt exec task

--- IOT-project2/src/task/api/StdExecTask.h
+++ IOT-project2/src/task/api/StdExecTask.h
@@ -3,6 +3,10 @@
 
 #include "task/api/Task.h"
 
+#include "ArduinoStandardLibrary.h"
+#include "Components/Door/Api/Door.h"
+#include <LiquidCrystal_I2C.h>
+
 enum StdExecState{
     READY,
     OPENING,
@@ -14,8 +18,21 @@ class StdExecTask : public Task {
     
 private:
     StdExecState state;
+
+    Door door;
+    LiquidCrystal_I2C display;
+    DigitalInput openButton;
+    DigitalInput closeButton;
+    DigitalOutput ledGreen;
+    DigitalOutput ledRed;
+
 public:
-    StdExecTask();
+    StdExecTask(Door door,
+                LiquidCrystal_I2C display,
+                DigitalInput openButton,
+                DigitalInput closeButton,
+                DigitalOutput ledGreen,
+                DigitalOutput ledRed);
     void tick();
     void reset();
 };
--- IOT-project2/src/task/impl/StdExecTask.cpp
+++ IOT-project2/src/task/impl/StdExecTask.cpp
@@ -1,7 +1,18 @@
 #include "task/api/StdExecTask.h"
 
 
-StdExecTask ::StdExecTask(){
+StdExecTask ::StdExecTask(Door door,
+                          LiquidCrystal_I2C display,
+                          DigitalInput openButton,
+                          DigitalInput closeButton,
+                          DigitalOutput ledGreen,
+                          DigitalOutput ledRed)
+    : door(door),
+      display(display),
+      openButton(openButton),
+      closeButton(closeButton),
+      ledGreen(ledGreen),
+      ledRed(ledRed){
     this->type = STD_EXEC;
     this->state = READY;
 }
