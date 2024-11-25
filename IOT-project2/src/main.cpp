a17c52e3b450548fc861fa89172134fb6d9aaeb3
Filippo Massari <filippo.massari6@studio.unibo.it>
2024-11-25 12:26:36 +0100

    add i/o obj and task to main

--- IOT-project2/src/main.cpp
+++ IOT-project2/src/main.cpp
@@ -1,12 +1,38 @@
 #include <Arduino.h>
-#include "ArduinoStandardLibrary.h"
+
 #include "scheduler/api/Scheduler.h"
 
+#include "ArduinoStandardLibrary.h"
+#include "Components/Pir/Api/Pir.h"
+#include "Components/Sonar/Api/Sonar.h"
+#include "Components/Temperaturesensor/Api/TemperatureSensor.h"
+#include "Components/Door/Api/Door.h"
+#include <LiquidCrystal_I2C.h>
+
+#include "task/api/Task.h"
+#include "task/api/ManagerTask.h"
+#include "task/api/StdExecTask.h"
+
+// Componenti I/O
+Pir userDetector = Pir(9);
+Sonar levelDetector = Sonar(13, 12);
+Door door = Door(9);
+LiquidCrystal_I2C display = LiquidCrystal_I2C(0x27, 16, 2);
+DigitalInput openButton = DigitalInput(2, 500);
+DigitalInput closeButton = DigitalInput(3, 500);
+DigitalOutput ledGreen = DigitalOutput(5);
+DigitalOutput ledRed = DigitalOutput(4);
+TemperatureSensor tempSensor = TemperatureSensor(2);
+
+// Scheduler
 Scheduler scheduler;
 
+// Tasks
+ManagerTask managerTask(levelDetector, tempSensor, userDetector, scheduler.getTaskList());
+StdExecTask stdExecTask(door, display, openButton, closeButton, ledGreen, ledRed);
+
 void setup() {
-    Serial.begin(9600);
-    delay(2000);
+  Serial.begin(9600);
   // conto del tempo ottimale per i task
   scheduler.init(10); //inserire tempo ricavato opportunamente
 }
--- IOT-project2/src/scheduler/api/Scheduler.h
+++ IOT-project2/src/scheduler/api/Scheduler.h
@@ -2,12 +2,6 @@
 #define __SCHEDULER__
 
 #include "ArduinoStandardLibrary.h"
-#include "Components/Pir/Api/Pir.h"
-#include "Components/Sonar/Api/Sonar.h"
-#include "Components/Temperaturesensor/Api/TemperatureSensor.h"
-#include "Components/Door/Api/Door.h"
-#include <LiquidCrystal_I2C.h>
-
 #include "task/api/Task.h"
 
 #define MAX_TASKS 10
@@ -20,33 +14,13 @@ private:
   Task* taskList[MAX_TASKS];  
   SchedulerTimer timer;
 
-  // Componenti I/O
-  Pir userDetector; // Sensore PIR
-  Sonar levelDetector; // Sonar
-  Door door; // Servo-motore
-  LiquidCrystal_I2C display; // Display
-  DigitalInput openButton; // Pulsante OPEN
-  DigitalInput closeButton; // Pulsante CLOSE
-  DigitalOutput ledGreen; // LED verde
-  DigitalOutput ledRed; // LED rosso
-  TemperatureSensor tempSensor; // Sensore di temperatura
-
 public:
-  Scheduler()
-    : userDetector(9), 
-      levelDetector(13, 12),
-      display(0x27, 16, 2), 
-      openButton(2, 500),
-      closeButton(3, 500),
-      ledGreen(5),
-      ledRed(4),
-      tempSensor(2),
-      door(9)
-  {}
+  Scheduler();
 
   void init(int basePeriod);  
   bool addTask(Task* task);  
   void schedule();
+  Task** getTaskList();
 };
 
 #endif
