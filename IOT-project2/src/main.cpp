820497ca43a7972163107d3400bed16728df1876
Whatsm97 <gambacortagiuseppe@outlook.it>
2024-11-25 10:02:27 +0100

    main logic

--- IOT-project2/src/main.cpp
+++ IOT-project2/src/main.cpp
@@ -1,14 +1,9 @@
 #include <Arduino.h>
 #include "ArduinoStandardLibrary.h"
 #include "SerialManager.h"
-//#include "scheduler/api/Scheduler.h"
+// #include "scheduler/api/Scheduler.h"
 
-
-
-
-
-
-//Scheduler scheduler;
+// Scheduler scheduler;
 int i = 530;
 
 String s = "gambacorta";
@@ -17,49 +12,41 @@ bool start = false;
 SerialManager serialManager(9600);
 bool toggle = false;
 
-
-void setup() {
-   serialManager.init();
-  serialManager.addVariableToSend((byte*)&i, VarType::INT);
-  serialManager.addVariableToSend((byte*)&i2, VarType::INT);
-  serialManager.addVariableToSend(&s);
-  pinMode(2, OUTPUT);
-   //Serial.begin(9600);
-   digitalWrite(2, HIGH);
+void setup()
+{
+    serialManager.init();
+    serialManager.addVariableToSend((byte *)&i, VarType::INT);
+    serialManager.addVariableToSend((byte *)&i2, VarType::INT);
+    serialManager.addVariableToSend(&s);
+    pinMode(2, OUTPUT);
+    // Serial.begin(9600);
+    digitalWrite(2, HIGH);
 }
 
-void loop() {
-    while (!serialManager.isSerialAvailable()) {
-        Serial.println("Waiting for serial connection...");
-        delay(1000);
-    }
+void loop()
+{
 
-    
-    //scheduler.schedule();
+    // scheduler.schedule();
+    if (serialManager.isSerialAvailable())
+    {
 
-    serialManager.doHandshake();
-   //Serial.println(s.length());
-   // Serial.println(s.c_str());
-        serialManager.addDebugMessage("prova debug seriale");
-        serialManager.addDebugMessage("prova debug seriale");
-        serialManager.addEventMessage("prova evento seriale");
-        toggle = !toggle;
-        if (toggle){
-            serialManager.addDebugMessage("prova debug seriale che capita ogni tanto");
-        }
-  
+        serialManager.doHandshake();
+
         serialManager.sendData();
         s = "gambacorta " + String(i);
         delay(1000);
-        
-    
+    }
 
-      digitalWrite(2, !start);
+    serialManager.addDebugMessage("prova debug seriale");
+    serialManager.addDebugMessage("prova debug seriale");
+    serialManager.addEventMessage("prova evento seriale");
+    toggle = !toggle;
+    if (toggle)
+    {
+        serialManager.addDebugMessage("prova debug seriale che capita ogni tanto");
+    }
 
-    
+    digitalWrite(2, !start);
 
-    i+=5;
-
-
-
+    i += 5;
 }
