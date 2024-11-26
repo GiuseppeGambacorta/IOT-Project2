18028a9eca4ab5ee52f37fa78b9ed024cd70723a
Filippo Massari <filippo.massari6@studio.unibo.it>
2024-11-26 15:39:29 +0100

    refactoring of manager impl

--- IOT-project2/src/task/impl/ManagerTask.cpp
+++ IOT-project2/src/task/impl/ManagerTask.cpp
@@ -1,93 +1,56 @@
 #include "task/api/ManagerTask.h"
 
-#define MAXTEMPTIME 10000
-
-ManagerTask::ManagerTask(Sonar& levelDetector,
-                         TemperatureSensor& tempSensor,
-                         Pir& userDetector,
-                         Task* taskList[MAX_TASKS])
-    : levelDetector(levelDetector),
-      tempSensor(tempSensor), 
-      userDetector(userDetector) {
-  tempAlarm = false;
-  levelAlarm = false;
-  userStatus = true;
-  tempTimer = new Timer(MAXTEMPTIME);
-  userTimer = new Timer(TSleep);
-  for (int i = 0; i < MAX_TASKS; i++) {
-    this->taskList[i] = taskList[i];
-  }
-}
-
 void ManagerTask::tick() {
+    int level = levelDetector.readDistance();
+    int temp = tempSensor.readTemperature();
+    bool user = userDetector.isDetected();
 
-  int level = levelDetector.readDistance();
-  int temp = tempSensor.readTemperature();
-  bool user = userDetector.isDetected();
-
-  if (level < LEVEL_MAX) {
-    levelAlarm = true;
-  } else {
-    levelAlarm = false;
-  }
-
-  tempTimer->active(temp > TEMP_MAX);
-  if (tempTimer->isTimeElapsed()) {
-    tempAlarm = true;
-  } else {
-    tempAlarm = false;
-  }
-
-  userTimer->active(user);
-  if (userTimer->isTimeElapsed()) {
-    userStatus = false;
-  } else {
-    userStatus = true;
-  }
-
-  if (levelAlarm) {
-    for (int i = 0; i < MAX_TASKS; i++) {
-      if (taskList[i] != nullptr) {
-        taskList[i]->setActive(false);
-        if (taskList[i]->getType() == ALLARM_LEVEL) {
-          taskList[i]->setActive(true);
-        }
-      }
+    // Gestione degli allarmi e degli stati
+    if (level < LEVEL_MAX) {
+        levelAlarm = true;
+    } else {
+        levelAlarm = false;
     }
-  } else if (tempAlarm) {
-    for (int i = 0; i < MAX_TASKS; i++) {
-      if (taskList[i] != nullptr) {
-        taskList[i]->setActive(false);
-        if (taskList[i]->getType() == ALLARM_TMP) {
-          taskList[i]->setActive(true);
-        }
-      }
+
+    tempTimer->active(temp > TEMP_MAX);
+    if (tempTimer->isTimeElapsed()) {
+        tempAlarm = true;
+    } else {
+        tempAlarm = false;
     }
-  } else if (!userStatus) {
-    for (int i = 0; i < MAX_TASKS; i++) {
-      if (taskList[i] != nullptr) {
-        taskList[i]->setActive(false);
-        if (taskList[i]->getType() == SLEEP) {
-          taskList[i]->setActive(true);
-        }
-      }
+
+    userTimer->active(user);
+    if (userTimer->isTimeElapsed()) {
+        userStatus = false;
+    } else {
+        userStatus = true;
     }
-  } else {
-    for (int i = 0; i < MAX_TASKS; i++) {
-      if (taskList[i] != nullptr) {
-        taskList[i]->setActive(false);
-        if (taskList[i]->getType() == STD_EXEC) {
-          taskList[i]->setActive(true);
-        }
-      }
+
+    // Selezione del task attivo
+    TaskType activeTaskType = STD_EXEC; // Default task
+
+    if (levelAlarm) {
+        activeTaskType = ALLARM_LEVEL;
+    } else if (tempAlarm) {
+        activeTaskType = ALLARM_TMP;
+    } else if (!userStatus) {
+        activeTaskType = SLEEP;
     }
-  }
+
+    // Attivazione del task corrispondente
+    for (int i = 0; i < MAX_TASKS; i++) {
+        if (taskList[i] != nullptr) {
+            taskList[i]->setActive(taskList[i]->getType() == activeTaskType);
+        }
+    }
 }
 
 void ManagerTask::reset() {
-  tempAlarm = false;
-  levelAlarm = false;
-  userStatus = true;
-  tempTimer = new Timer(MAXTEMPTIME);
-  userTimer = new Timer(TSleep);
+    tempAlarm = false;
+    levelAlarm = false;
+    userStatus = true;
+    delete tempTimer;
+    delete userTimer;
+    tempTimer = new Timer(MAXTEMPTIME);
+    userTimer = new Timer(TSleep);
 }
