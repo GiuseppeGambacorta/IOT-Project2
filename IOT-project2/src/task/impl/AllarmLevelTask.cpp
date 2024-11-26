a7cbb74d7fb2f4c1f5b9701e204b9660198e1d25
Enrico Cornacchia <enrico.cornacchia@studio.unibo.it>
2024-11-26 15:45:58 +0100

    first iteration of the AllarmLevelTask

---
+++ IOT-project2/src/task/impl/AllarmLevelTask.cpp
@@ -0,0 +1,29 @@
+#include "ArduinoStandardLibrary.h"
+#include "task/api/AllarmLevelTask.h"
+
+AllarmLevelTask::AllarmLevelTask(Door& door,
+                                 LiquidCrystal_I2C& display,
+                                 DigitalOutput& ledGreen,
+                                 DigitalOutput& ledRed) 
+                                 : door(door), display(display), ledGreen(ledGreen), ledRed(ledRed) {
+    this->type = ALLARM_LEVEL;
+}
+
+void AllarmLevelTask::tick() {
+    display.setCursor(0, 0);
+    display.print("CONTAINER FULL");
+    ledGreen.turnOff();
+    ledRed.turnOn();
+    if (door.isOpened()) {
+        door.close();
+    }
+    // a sleep could be used to turn off the system
+    // until the operator presses "Empty the container" on the GUI
+}
+
+void AllarmLevelTask::reset() {
+    ledGreen.turnOn();
+    ledRed.turnOff();
+    display.clear();
+    // should it open the door in reverse here?
+}
