c68efd32316397eaab80ebaa21ec7e005bd165b6
Filippo Massari <filippo.massari6@studio.unibo.it>
2024-11-29 15:41:05 +0100

    add stable version of manager eand main

--- IOT-project2/src/task/WasteDisposal/impl/WasteDisposalTask.cpp
+++ IOT-project2/src/task/WasteDisposal/impl/WasteDisposalTask.cpp
@@ -2,33 +2,87 @@
 
 
 
-WasteDisposalTask::WasteDisposalTask(
-                      EmptyBinTask& emptyBinTask,
-                      HomingTask& homingTask,
-                      SleepTask& sleepTask,
-                      StdExecTask& stdExecTask
-                     )
-            : emptyBinTask(emptyBinTask),
-              homingTask(homingTask),
-              sleepTask(sleepTask),
-              stdExecTask(stdExecTask)
-     {}
-
+WasteDisposalTask::WasteDisposalTask(StdExecTask& stdExecTask,
+                                     AlarmLevelTask& alarmLevelTask,
+                                     AlarmTempTask& alarmTempTask,
+                                     Sonar& levelDetector,
+                                     TemperatureSensor& tempSensor)
+            :stdExecTask(stdExecTask),
+            alarmLevelTask(alarmLevelTask),
+            alarmTempTask(alarmTempTask),
+            state(WasteDisposalState::STD_EXEC),
+            tempTimer(Timer(MAXTEMPTIME)),
+            levelDetector(levelDetector),
+            tempSensor(tempSensor){}
 
 void WasteDisposalTask::tick() {
     int level = levelDetector.readDistance();
     int temp = tempSensor.readTemperature();
-    bool user = userDetector.isDetected();
 
-    this->homingTask.active(false);
-    this->stdExecTask.active(false);
-    this->sleepTask.active(false);
-    this->emptyBinTask.active(false);
+    switch (state){
+    case STD_EXEC:
+        if (level >= LEVEL_MAX) {
+            state = WasteDisposalState::LVL_ALLARM;
+        }
+        if (temp >= TEMP_MAX) {
+            tempTimer.active(true);
+        } else {
+            tempTimer.active(false);
+            tempTimer.reset();
+        }
+        if (tempTimer.isTimeElapsed()) {
+            state = WasteDisposalState::TEMP_ALLARM;
+        }
+        break;
+    case LVL_ALLARM:
+        if (level < LEVEL_MAX) {
+            state = WasteDisposalState::STD_EXEC;
+        }
+        if (temp > TEMP_MAX) {
+            tempTimer.active(true);
+        } else {
+            tempTimer.active(false);
+            tempTimer.reset();
+        }
+        if (tempTimer.isTimeElapsed()) {
+            state = WasteDisposalState::TEMP_ALLARM;
+        }
+        break;
+    case TEMP_ALLARM:
+        if (temp < TEMP_MAX) {
+            tempTimer.active(false);
+            tempTimer.reset();
+            state = WasteDisposalState::STD_EXEC;
+        }
+        if (level >= LEVEL_MAX) {
+            state = WasteDisposalState::LVL_ALLARM;
+        }
+        break;
+    }
+
+    switch (state){
+        case STD_EXEC:
+            stdExecTask.setActive(true);
+            alarmLevelTask.setActive(false);
+            alarmTempTask.setActive(false);
+            break;
+        case LVL_ALLARM:
+            stdExecTask.setActive(false);
+            alarmLevelTask.setActive(true);
+            alarmTempTask.setActive(false);
+            break;
+        case TEMP_ALLARM:
+            stdExecTask.setActive(false);
+            alarmLevelTask.setActive(false);
+            alarmTempTask.setActive(true);
+            break;
+    }
 }
 
 void WasteDisposalTask::reset() {
-    this->stdExecTask.reset();
-    this->emptyBinTask.reset();
-    this->sleepTask.reset();
-    this->state = WasteDisposalState::Homing;
+    state = WasteDisposalState::STD_EXEC;
+    //tempTimer.reset();
+    stdExecTask.setActive(true);
+    alarmLevelTask.setActive(false);
+    alarmTempTask.setActive(false);
 }
