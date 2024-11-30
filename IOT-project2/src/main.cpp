56b20411f1bfef99d6a7056f7fc67aef464af4eb
Whatsm97 <gambacortagiuseppe@outlook.it>
2024-11-30 16:44:55 +0100

    com working

--- IOT-project2/src/main.cpp
+++ IOT-project2/src/main.cpp
@@ -22,6 +22,7 @@
 //#define SECURITY_MARGIN (maxTime/10)
 
 // Componenti I/O
+/*
 Pir userDetector = Pir(8);
 Sonar levelDetector = Sonar(13, 12);
 Door door = Door(9);
@@ -39,9 +40,9 @@ AlarmTempTask alarmTempTask(ledGreen, ledRed, display, door, tempSensor);
 OutputTask outputTask(door, display, ledGreen, ledRed);
 
 WasteDisposalTask wasteDisposalTask (stdExecTask, alarmLevelTask, alarmTempTask, levelDetector, tempSensor);
-
+*/
 // Scheduler
-Scheduler scheduler;
+
 
 /*unsigned long calculateOptimalPeriod(Scheduler& scheduler) {
     unsigned long maxTime = 0;
@@ -64,10 +65,18 @@ Scheduler scheduler;
 }*/
 
 
+SerialInputTask serialinputTask;
+SerialOutputTask serialoutputTask;
+ProvaTask provaTask;
+SerialManager& serialManager = ServiceLocator::getSerialManagerInstance();
+Scheduler scheduler;
+
+
 void setup() {
-  Serial.begin(9600);
+
 
   //init task
+  /*
   inputTask.init(20);
   wasteDisposalTask.init(20);
   stdExecTask.init(20);
@@ -82,9 +91,24 @@ void setup() {
   scheduler.addTask(&alarmTempTask);
   scheduler.addTask(&stdExecTask);
   scheduler.addTask(&outputTask);
+
   
   // init scheduler
   scheduler.init(150);
+  */   
+    serialManager.init();
+    scheduler.init(25);
+    scheduler.addTask(&serialinputTask);
+    scheduler.addTask(&serialoutputTask);
+    scheduler.addTask(&provaTask);
+    serialoutputTask.setActive(true);
+    serialinputTask.setActive(true);
+    provaTask.setActive(true);
+    serialinputTask.init(500);
+    serialoutputTask.init(250);
+    provaTask.init(50);
+
+
 
 }
 
